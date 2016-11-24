02c26dbaf8080d9df0cee4730df7c08d54f28672
HappyJie135588 <361125241@qq.com>
2016-11-24 23:15:52 +0800

    indicator添加完成

--- app/src/main/java/com/huangjie/googleplay/MainActivity.java
+++ app/src/main/java/com/huangjie/googleplay/MainActivity.java
@@ -1,13 +1,118 @@
 package com.huangjie.googleplay;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.huangjie.googleplay.utils.UIUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;//抽屉开关控件
+    private PagerSlidingTabStrip mTabStrip;
+    private ViewPager mPager;
+    private String[] mTitles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initToolbar();
+        initData();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (mDrawerToggle.onOptionsItemSelected(item)) {
+                    return true;
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initView() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+        mTabStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        mPager = (ViewPager) findViewById(R.id.main_pager);
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setTitle("GooglePlay");
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        mDrawerToggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout,
+                R.string.main_des_drawer_open,
+                R.string.main_des_drawer_close);
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    private void initData() {
+        mTitles = UIUtils.getStringArray(R.array.main_titles);
+        mPager.setAdapter(new MainPagerAdapter());
+        mTabStrip.setViewPager(mPager);
+    }
+
+    class MainPagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            if(mTitles!=null){
+                return mTitles.length;
+            }
+            return 0;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            TextView tv = new TextView(UIUtils.getContext());
+            tv.setText(mTitles[position]);
+            container.addView(tv);
+            return tv;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if (mTitles != null) {
+                return mTitles[position];
+            }
+            return super.getPageTitle(position);
+        }
+    }
+
 }
